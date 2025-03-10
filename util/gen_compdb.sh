c26b6145ed1ea94ab7a4e4d21b37d1936737ee09
Chuyang Chen <chuyangchen2018@outlook.com>
2025-03-10 22:11:43 +0000

    Switch to LLVM 20

--- util/gen_compdb.sh
+++ util/gen_compdb.sh
@@ -8,6 +8,6 @@ if [ ! -d "$BUILD_DIR" ]; then
     mkdir -p $BUILD_DIR     
 fi
 
-cmake -G Ninja -B $BUILD_DIR -S .
+cmake -G Ninja -DZ3_TRUST_SYSTEM_VERSION=on -DSYMCC_RT_BACKEND=simple -DLLVM_DIR=/llvm_source/build -B $BUILD_DIR -S .
 ninja -C $BUILD_DIR -t compdb > compile_commands.json
 ninja -C $BUILD_DIR/SymCCRuntime-prefix/src/SymCCRuntime-build -t compdb >> runtime/compile_commands.json
